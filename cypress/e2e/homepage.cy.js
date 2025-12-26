07676cf5bda50e0911934275e194a7a4fe358887
kamal <chhettrikamal09@gmail.com>
2025-12-26 17:30:22 +0545

    homepage items info

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -7,15 +7,37 @@ describe('Testing the homepage feature ', ()=>{
        cy.get('[data-qa="login-email"]').type('rajeshdai@gmail.com')
        cy.get('[data-qa="login-password"]').type('Admin@1234')
        cy.get('[data-qa="login-button"]').click()
-       
+    })
 
        it('To Test, The banner of homepage ' , ()=>{
-        cy.url(),should('include','/')
+        cy.url().should('include','/')
         cy.get('#slider').should('be.visible')
+        cy.get('[class="left control-carousel hidden-xs"]').should('be.visible').then(($btn)=>{
+            for (let i = 0; i < 3; i++) {
+            cy.wrap($btn).click()
+            }
+
+        }) 
+        cy.get('[class="right control-carousel hidden-xs"]').should('be.visible').then(($btn)=>{
+            for (let i = 0; i < 3; i++) {
+            cy.wrap($btn).click()
+            }
+
+        }) 
+        cy.get('[class="productinfo text-center"]').should('have.length', 40)
+        cy.get('[id="accordian"]').should('be.visible')
+        // cy.get('[href="#Women"]').should('be.visible').click()
+        })
+     it('To test, View the product', ()=>{
+        cy.get('a[href="/product_details/39"]').click()
+        cy.url().should('include','/product_details/39')
+        cy.get('[class="newarrival"]').should('be.visible')
+        cy.get("div[class='product-information'] h2").should('be.visible')
+        cy.get("body > section:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > p:nth-child(3)")
+         // cy.contains('Cotton Silk Hand Block Print Saree Top').should('be.visible')
+             
+         })
 
 
-       })
-
-
-    })
+    
 })
